56d4f2001e546d80ada2609c5bc7cf3226384d6d
DereSundayT <toluwanisundaydere@gmail.com>
2020-05-03 00:48:08 +0100

    modify app js file 2

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-//import WorkingWithTextInput  from './workingwithtextinput/App';
-import ListViewScrollView  from './workingWithListViewAndScrollView/App'
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Hello</Text>
       
     </View>
   );
