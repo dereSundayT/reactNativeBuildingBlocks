82c47341307596bcb5f8dce9c66312b77a050941
DereSundayT <toluwanisundaydere@gmail.com>
2020-05-02 21:02:03 +0100

    working with TouchableComponent

---
+++ workingWithTouchableComponent/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View,StyleSheet} from 'react-native';
+
+const WorkingWithTouchableComponent = () => {
+    return(
+        <View>
+            
+
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+
+    }
+});
+
+export default WorkingWithTouchableComponent;
