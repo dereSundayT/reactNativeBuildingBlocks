b1e37a49d3395550e1e8c0dfca4d275893a373e8
DereSundayT <toluwanisundaydere@gmail.com>
2020-05-03 00:45:10 +0100

    modify app js file

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import ListViewScrollView  from './workingWithListViewAndScrollView/App'
 export default function App() {
   return (
     <View style={styles.container}>
-      <ListViewScrollView />
+      
     </View>
   );
 }
